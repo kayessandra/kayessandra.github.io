66e4ad0d39978cde06a2b5da92dbe9ccbbe29935
Kassandra Casalla <kaye@autodeal.com.ph>
2021-02-12 15:02:30 +0800

    added gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+// const deploy = require('gulp-gh-pages');
+const gulp = require('gulp');
+const sass = require('gulp-dart-sass');
+const browsersync = require("browser-sync").create();
+
+function css() {
+	return gulp.src('app/scss/**/*.scss')
+	.pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browsersync.stream());
+}
+
+// function deploy() {
+// 	return gulp.src("./dist/**/*")
+//     .pipe(deploy())
+// }
+
+function watch() {
+	browsersync.init({
+	    server: {
+		  baseDir: ["."],
+		  index: ['index.html'],
+		  directory: true
+	    },
+	});
+
+	gulp.watch('app/scss/**/*.scss', css);
+	gulp.watch('*.html').on('change', browsersync.reload);
+	gulp.watch('app/**/*.js', browsersync);
+}
+
+exports.css = css;
+exports.watch = watch;
